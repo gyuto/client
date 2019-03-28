94e9c47ca465ec2d196a7bb8307e79e0f8bd9739
Di Ye <di.ye@buildingconnected.com>
2019-03-27 22:36:14 -0700

    Update header

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,40 @@
+import React from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import logo from "../images/gyuto-logo.png"
 
 const Header = ({ siteTitle }) => (
   <header
     className="bg-scarlet"
     style={{
-      textAlign: "center",
       marginBottom: `1.45rem`,
       borderBottom: "2px solid gold",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexWrap: "wrap",
     }}
   >
     <div
       style={{
-        margin: `0 auto`,
+        width: 150,
+        height: 150,
+      }}
+    >
+      <img
+        style={{
+          height: 130,
+          marginTop: 10,
+        }}
+        src={logo}
+        alt="Gyuto Wheel of Dharma Monastery"
+      />
+    </div>
+    <div
+      style={{
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
+        textAlign: "center",
       }}
     >
       <h1 style={{ margin: 0 }}>

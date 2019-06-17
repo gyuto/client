bc964543900c3dd49f9d2572ee718129a95aa881
Di Ye <imdiye@gmail.com>
2019-06-16 19:03:24 -0700

    add flyer

--- src/components/events/constants.js
+++ src/components/events/constants.js
@@ -1,33 +1,34 @@
-import longLing from "../../images/long-ling.jpg"
-import center from "../../images/centerExterior.jpg"
+import longLing from '../../images/long-ling.jpg'
+import center from '../../images/centerExterior.jpg'
+import flyer from '../../images/amitayus-flyer.jpeg'
 
 export const events = [
-  {
-    name: "Our 25th Anniversary",
-    date: "Saturday, June 29, 2019",
-    time: "10 am - 4 pm",
-    location: {
-      address: "2601 Taylor St. NE, Minneapolis, MN 55418",
-      googleMapLink:
-        "https://www.google.com/maps/place/2601+NE+Taylor+St,+Minneapolis,+MN+55418/@45.015186,-93.245637,17z/data=!3m1!4b1!4m5!3m4!1s0x52b32dc224fbe3cb:0x9641501349061f2c!8m2!3d45.015186!4d-93.243443",
-    },
-    moreInfo: `
+	{
+		name: 'Our 25th Anniversary',
+		date: 'Saturday, June 29, 2019',
+		time: '5 pm - 8 pm',
+		location: {
+			address: '1900 7th St., New Brighton, MN 55112',
+			googleMapLink:
+				'https://www.google.com/maps/place/Christ+the+King+Lutheran+Church/@45.061937,-93.2142977,17z/data=!3m1!4b1!4m5!3m4!1s0x52b32ee72ee90a33:0xb3a97dd651d2542e!8m2!3d45.061937!4d-93.212109',
+		},
+		moreInfo: `
     Gyuto Wheel of Dharma Monastery will be celebrating it’s 25th Anniversary. Everyone is invited. Phone number: (612) 724-3124. Email: gyuto.us@gmail.com.
     `,
-    pic: center,
-  },
-  {
-    name: "Amitayus Long Life Initiation by H.E. Ling Rinpoche",
-    date: "Sunday, June 30, 2019",
-    time: "10 am - 12 pm",
-    location: {
-      address: "2601 Taylor St. NE, Minneapolis, MN 55418",
-      googleMapLink:
-        "https://www.google.com/maps/place/2601+NE+Taylor+St,+Minneapolis,+MN+55418/@45.015186,-93.245637,17z/data=!3m1!4b1!4m5!3m4!1s0x52b32dc224fbe3cb:0x9641501349061f2c!8m2!3d45.015186!4d-93.243443",
-    },
-    moreInfo: `
+		pic: center,
+	},
+	{
+		name: 'Amitayus Long Life Initiation by H.E. Ling Rinpoche',
+		date: 'Sunday, June 30, 2019',
+		time: '5 pm - 8 pm',
+		location: {
+			address: '1900 7th St., New Brighton, MN 55112',
+			googleMapLink:
+				'https://www.google.com/maps/place/Christ+the+King+Lutheran+Church/@45.061937,-93.2142977,17z/data=!3m1!4b1!4m5!3m4!1s0x52b32ee72ee90a33:0xb3a97dd651d2542e!8m2!3d45.061937!4d-93.212109',
+		},
+		moreInfo: `
     Amitayus Long Ling Invited by His Eminence Ling Rinpoche. Everyone is welcome and it’s free to the public. Please avoid black food before the invitation such as meet, alcohol, garlic and eggs.
     `,
-    pic: longLing,
-  },
+		pic: longLing,
+	},
 ]
